1eff420351fcba5818755b5594091c0c22176f35
spacemanspumoni <sincerelyxyz414@gmail.com>
2025-09-18 16:31:58 -0500

    add update-electron-app dependency and initialize in main process

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
+import { updateElectronApp } from 'update-electron-app';
+
+updateElectronApp();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
